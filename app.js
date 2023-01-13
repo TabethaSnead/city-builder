1ee56341fdbbe6986dd4fe52e3d1a3b67ab93e5b
TabethaSnead <tabetha.snead@gmail.com>
2023-01-12 17:43:27 -0800

    grabbed all the elements from html in javascript

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 /* Imports */
 
 /* Get DOM Elements */
-
+const mainDropdown = document.getElementById('main-dropdown');
+const sideDropdown = document.getElementById('side-dropdown');
+const dessertDropdown = document.getElementById('');
+const favoriteInput = document.getElementById('favorite-input');
+const favoriteButton = document.getElementById('favorite-button');
+const stats = document.getElementById('stats');
+const numOfChoices = document.getElementById('numOfChoices');
+const favoriteResults = document.getElementById('favoriteResults');
+const mainResult = document.getElementById('mainResult');
+const sideResult = document.getElementById('sideResult');
+const dessertResult = document.getElementById('dessertResult');
 /* State */
 
 /* Events */
